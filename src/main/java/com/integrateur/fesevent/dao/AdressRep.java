8066ca3c917ed824d323b371abb92cf68fba1954
ilyass_filali <ilyassfilali6@gmail.com>
2021-01-27 21:00:43 +0100

    updating Rep

--- src/main/java/com/integrateur/fesevent/dao/AdressRep.java
+++ src/main/java/com/integrateur/fesevent/dao/AdressRep.java
@@ -1,9 +1,11 @@
 package com.integrateur.fesevent.dao;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.integrateur.fesevent.modules.RestaurantAdress;
 
 public interface AdressRep extends CrudRepository<RestaurantAdress, Integer> {
-
+	Optional<RestaurantAdress> findByadress(String adress);
 }
